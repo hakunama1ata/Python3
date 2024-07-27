ffcdcb2a96cdbfa72b6f02f373b0ace9a8e91b41
Akshay Vinod <67356841+hakunama1ata@users.noreply.github.com>
2024-07-27 10:41:30 +0200

    Optimized the code

--- Kniematics.py
+++ Kniematics.py
@@ -1,99 +1,55 @@
 import math
 
-what_to_find=input("What should i find for you ? a= acceleration, s=distance, u=initial velocity, v=final velocity, t=time taken")
-what_to_find.lower
-parameters=set(['a','s','t','u','v'])
-while what_to_find not in parameters:
-    print("unknown parameter, please enter the correct one!")
-    what_to_find=input("What should i find for you ? a= acceleration, s=distance, u=initial velocity, v=final velocity, t=time taken")
+def get_value(prompt):
+    return float(input(prompt))
 
-print("What are the given values?")
-if what_to_find=='a':
-    v=float(input("Please enter the value of final velocity"))
-    u=float(input("Please enter the value of initial velocity"))
-    s=float(input("Please enter the value of displacement"))
-    t=float(input("Please enter the value of time"))
-    t=(v-u)/a
-    u=v-(a*t)
-    u=(s-((a*(t*t)/2)))/t
-    u=(math.sqrt((v*v)-(2*a*s)))
-    s=((u*t)+((1/2)*a*t*t))
-    s=((v*v)-(u*u))/(2*a)
-    v=(math.sqrt((u*u)+(2*a*s)))
-    v=u+(a*t)
-    a=float((v-u)/t)
-    a=((v*v)-(u*u)/(2*s))
-    a=(2*(s-(u*t)))/(t*t)
-    print("The acceleration is",a)
-elif what_to_find=='v':
-    u=float(input("Please enter the value of initial velocity"))
-    s=float(input("Please enter the value of displacement"))
-    t=float(input("Please enter the value of time"))
-    a=float(input("Please enter the value of acceleration"))
-    t=(v-u)/a
-    u=v-(a*t)
-    u=(s-((a*(t*t)/2)))/t
-    u=(math.sqrt((v*v)-(2*a*s)))
-    s=((u*t)+((1/2)*a*t*t))
-    s=((v*v)-(u*u))/(2*a)
-    v=(math.sqrt((u*u)+(2*a*s)))
-    v=u+(a*t)
-    a=float((v-u)/t)
-    a=((v*v)-(u*u)/(2*s))
-    a=(2*(s-(u*t)))/(t*t)
-    print("The final velocity is",v)
+def calculate_unknown_value(unknown):
+    if unknown == 'a':
+        v = get_value("Please enter the value of final velocity: ")
+        u = get_value("Please enter the value of initial velocity: ")
+        t = get_value("Please enter the value of time: ")
+        a = (v - u) / t
+        print("The acceleration is", a)
     
-elif what_to_find=='u':
-    v=float(input("Please enter the value of final velocity"))
-    s=float(input("Please enter the value of displacement"))
-    t=float(input("Please enter the value of time"))
-    a=float(input("Please enter the value of acceleration"))
-    t=(v-u)/a
-    u=v-(a*t)
-    u=(s-((a*(t*t)/2)))/t
-    u=(math.sqrt((v*v)-(2*a*s)))
-    s=((u*t)+((1/2)*a*t*t))
-    s=((v*v)-(u*u))/(2*a)
-    v=(math.sqrt((u*u)+(2*a*s)))
-    v=u+(a*t)
-    a=float((v-u)/t)
-    a=((v*v)-(u*u)/(2*s))
-    a=(2*(s-(u*t)))/(t*t)
-    print("The initial velocity is",u)
-elif what_to_find=='t':
-    v=float(input("Please enter the value of final velocity"))
-    s=float(input("Please enter the value of displacement"))
-    a=float(input("Please enter the value of acceleration"))
-    u=float(input("Please enter the value of initial velocity"))
-    t=(v-u)/a
-    u=v-(a*t)
-    u=(s-((a*(t*t)/2)))/t
-    u=(math.sqrt((v*v)-(2*a*s)))
-    s=((u*t)+((1/2)*a*t*t))
-    s=((v*v)-(u*u))/(2*a)
-    v=(math.sqrt((u*u)+(2*a*s)))
-    v=u+(a*t)
-    a=float((v-u)/t)
-    a=((v*v)-(u*u)/(2*s))
-    a=(2*(s-(u*t)))/(t*t)
-    print("The time is",t)
+    elif unknown == 'v':
+        u = get_value("Please enter the value of initial velocity: ")
+        a = get_value("Please enter the value of acceleration: ")
+        t = get_value("Please enter the value of time: ")
+        v = u + (a * t)
+        print("The final velocity is", v)
+    
+    elif unknown == 'u':
+        v = get_value("Please enter the value of final velocity: ")
+        a = get_value("Please enter the value of acceleration: ")
+        t = get_value("Please enter the value of time: ")
+        u = v - (a * t)
+        print("The initial velocity is", u)
+    
+    elif unknown == 't':
+        v = get_value("Please enter the value of final velocity: ")
+        u = get_value("Please enter the value of initial velocity: ")
+        a = get_value("Please enter the value of acceleration: ")
+        t = (v - u) / a
+        print("The time taken is", t)
+    
+    elif unknown == 's':
+        u = get_value("Please enter the value of initial velocity: ")
+        v = get_value("Please enter the value of final velocity: ")
+        a = get_value("Please enter the value of acceleration: ")
+        s = ((v * v) - (u * u)) / (2 * a)
+        print("The displacement is", s)
 
-else:
-    a=float(input("Please enter the value of acceleration"))
-    v=float(input("Please enter the value of final velocity"))
-    u=float(input("Please enter the value of initial velocity"))
-    t=float(input("Please enter the value of time"))
-    t=(v-u)/a
-    u=v-(a*t)
-    u=(s-((a*(t*t)/2)))/t
-    u=(math.sqrt((v*v)-(2*a*s)))
-    s=((u*t)+((1/2)*a*t*t))
-    s=((v*v)-(u*u))/(2*a)
-    v=(math.sqrt((u*u)+(2*a*s)))
-    v=u+(a*t)
-    a=float((v-u)/t)
-    a=((v*v)-(u*u)/(2*s))
-    a=(2*(s-(u*t)))/(t*t)
-    print("The value of displacement is",s)
+def main():
+    parameters = {'a', 's', 't', 'u', 'v'}
+    
+    what_to_find = input("What should I find for you? a= acceleration, s= distance, u= initial velocity, v= final velocity, t= time taken: ").lower()
+    
+    while what_to_find not in parameters:
+        print("Unknown parameter, please enter the correct one!")
+        what_to_find = input("What should I find for you? a= acceleration, s= distance, u= initial velocity, v= final velocity, t= time taken: ").lower()
+    
+    print("What are the given values?")
+    calculate_unknown_value(what_to_find)
 
-
+if __name__ == "__main__":
+    main()
